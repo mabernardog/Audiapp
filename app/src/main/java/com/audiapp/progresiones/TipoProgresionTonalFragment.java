e055440a897c984e9ec5d1aa1e4803512066027d
mabernardog <mabg181093@usal.es>
2019-04-13 13:59:56 +0200

    Terminada funcionalidad de TipoProgresionTonalFragment

--- app/src/main/java/com/audiapp/progresiones/TipoProgresionTonalFragment.java
+++ app/src/main/java/com/audiapp/progresiones/TipoProgresionTonalFragment.java
@@ -1,28 +1,49 @@
-
 package com.audiapp.progresiones;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TableLayout;
+import android.widget.ToggleButton;
 
 import com.audiapp.R;
+
+import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import utils.OperacionesTablaBotones;
 
 
 public class TipoProgresionTonalFragment extends Fragment {
+
+    @Nullable
+    @BindView(R.id.tablaTiposProgresionLineal)
+    TableLayout mTableLayout;
+    private ToggleButton.OnCheckedChangeListener mOnCheckedChangeListener;
+    public ProgresionTonalViewModel mViewModel;
 
     public static TipoProgresionTonalFragment newInstance() {
         return new TipoProgresionTonalFragment();
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        assert getParentFragment() != null;
+        mViewModel = ViewModelProviders.of(getParentFragment()).get(ProgresionTonalViewModel.class);
+    }
+
+    @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        // TODO: Use the ViewModel
     }
 
     @Override
@@ -30,7 +51,51 @@ public class TipoProgresionTonalFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         // Inflar la vista
         View vistaFragmento = inflater.inflate(R.layout.fragment_tipo_progresion_tonal, container, false);
+        ButterKnife.bind(this, vistaFragmento);
+        ArrayList<String> activos = OperacionesTablaBotones.getBotonesActivos(mTableLayout);
+        // Crear el listener para los botones
+        mOnCheckedChangeListener = (buttonView, isChecked) -> {
+            // Si estará activo
+            if (isChecked) {
+                // Mostrarlo en negrita
+                buttonView.setTypeface(null, Typeface.BOLD);
+                // Añadirlo al view model si no estaba ya
+                boolean loContiene = false;
+                for(String texto : mViewModel.getTiposProgresion()) {
+                    if(texto.contentEquals(buttonView.getText())) {
+                        loContiene = true;
+                        break;
+                    }
+                }
+                if(!loContiene) mViewModel.getTiposProgresion().add((String) buttonView.getText());
+            } else {    // Si no
+                // Buscarlo en el view model y eliminarlo
+                int i = 0;
+                for(String tipo : mViewModel.getTiposProgresion()) {
+                    if(tipo.contentEquals(buttonView.getText())) {
+                        mViewModel.getTiposProgresion().remove(i);
+                        break;
+                    }
+                    i++;
+                }
+            }
+        };
+        assert mTableLayout != null;
+        ArrayList<CheckBox> botones = OperacionesTablaBotones.getBotonesDeTabla(mTableLayout);
+        for(CheckBox boton : botones) {
+            boton.setOnCheckedChangeListener(mOnCheckedChangeListener);
+        }
+        // Recorrer el view model y activar los botones que toquen
+        for(String tipo : mViewModel.getTiposProgresion()) {
+            for(CheckBox boton : botones) {
+                if(tipo.contentEquals(boton.getText())) {
+                    boton.setChecked(true);
+                    break;
+                }
+            }
+        }
         return vistaFragmento;
     }
+
 
 }
